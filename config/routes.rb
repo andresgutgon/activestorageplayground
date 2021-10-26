1bdd87c1cb587b627378f27daccedfdf91c4a353
andresgutgon <andresgutgon@gmail.com>
2021-10-26 15:23:42 +0200

    Use a CDN route for active storage images uploaded to a remote bucket

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,28 @@ Rails.application.routes.draw do
     end
   end
 
+  direct :cdn_image do |model, options|
+    if model.respond_to?(:signed_id)
+      route_for(
+        :rails_service_blob_proxy,
+        model.signed_id,
+        model.filename,
+        options.merge(host: 'http://localhost:3000')
+      )
+    else
+      signed_blob_id = model.blob.signed_id
+      variation_key  = model.variation.key
+      filename       = model.blob.filename
+
+      route_for(
+        :rails_blob_representation_proxy,
+        signed_blob_id,
+        variation_key,
+        filename,
+        options.merge(host: 'http://localhost:3000')
+      )
+    end
+  end
+
   root 'my_gallery/galleries#show'
 end
